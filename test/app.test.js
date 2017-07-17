c27e612a97ee35aeb64697eaf34699bde5365c8a
Joe Churchman <churchman.joe@gmail.com>
2017-07-17 14:32:23 -0700

    initial project structure written, test fails

---
+++ test/app.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const assert = chai.assert;
+
+const app = require('../lib/app');
+
+describe('server', () => {
+  const request = chai.request(app);
+
+  it('says hello world', done => {
+    request.get('/')
+      .get((err, res) => {
+        if(err) return done(err);
+        assert.equal(res.text, 'hello world');
+        done();
+      });
+  });
+
+
+
+});
