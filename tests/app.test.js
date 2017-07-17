ccbc1ad34c80faa5178797db2fe4cf0f55b98fc4
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-07-17 15:15:36 -0700

    Passing Hello Stranger test

---
+++ tests/app.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const assert = chai.assert;
+
+const app = require('../lib/app');
+
+describe('/greeting', () => {
+    const request = chai.request(app);
+    it('responds with hello stranger if no name or salutation given', done => {
+        request.get('/greeting')
+            .end((err, res) => {
+                if (err) done(err);
+                assert.equal(res.text, 'Hello Stranger');
+                done();
+            });
+    });
+
+    xit('responds with hello name if name but no salutation given', done => {
+        request.get('/greeting/joe')
+            .end((err, res) => {
+                if (err) done(err);
+                assert.equal(res.url, 'hello joe');
+                done();
+            });
+    });
+});
