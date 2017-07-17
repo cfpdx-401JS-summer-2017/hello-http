da1928a25db05ab9e4231e55cb73146d813d1d8a
Christopher Thomson <christopher.thomson42@gmail.com>
2017-07-17 15:41:03 -0700

    functioning codde failing test

--- test/app.test.js
+++ test/app.test.js
@@ -1,22 +1,24 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const app = require('../lib/app.js');
 const assert = require('assert');
 // each of these inital 3 or 4 tests will be incorperating GETS meaning HTTP 200 responce codes 
 describe('/greeting', ()=>{
     const request = chai.request(app);
-    it('says hello', ()=>{
+    it('says hello', (done)=>{
         request.get('/greeting')
             .end((err,res)=> {
                 if (err) done(err);
-                assert.equal(res.test, 'hello stranger');
+                assert.equal(res.text, 'hello stranger');
                 done();
             });
     });
-    it('says hello person', ()=>{
+    it('says hello person', (done)=>{
         request.get('/greeting')
             .end((err,res)=> {
                 if (err) done(err);
-                assert.equal(res.test, 'hello stranger');
+                assert.equal(res.text, 'hello stranger');
                 done();
             });
     });
