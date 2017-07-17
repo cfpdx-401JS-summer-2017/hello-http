411298d288ac683431efab4db6e4ede65178121e
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-07-17 14:44:14 -0700

    first test pasted

---
+++ tests/app.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const assert = chai.assert;
+
+const app = require('../lib/app');
+
+describe('app', () =>{
+     const request = chai.request(app);
+    it('works', done => {
+    request.get('/greeting')
+    .end((err, res) => {
+        if (err) done(err);
+        assert.equal(res.text, 'hello world');
+        done();
+    });
+});
+ it('works', done => {
+        request.get('/greeting/joe')
+            .end((err, res) => {
+                if (err) done(err);
+                assert.equal(res.text, 'hello friend');
+                done();
+            });
+    });
+});
