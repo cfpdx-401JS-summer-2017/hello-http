b35a457c05d1c4b97704166ebe4f16b23b4842a7
Mark Petrie <petrie.mark@gmail.com>
2017-07-17 18:49:48 -0700

    Working greeting function and 3 passing tests

--- server.js
+++ server.js
@@ -1,35 +1,8 @@
 
 const http = require('http');
-const url = require('url');
-const app = require('app');
+const app = require('./lib/app');
 
-const server = http.createServer((req, res) => {
-
-
-    res.setHeader('Content-Type', 'text/html');
-
-    if (req.url === '/error') {
-        res.statusCode = 400;
-        res.statusMessage = 'Oh No!';
-        res.end('Warning!');
-    }
-    else {
-        res.statusCode = 200;
-        res.statusMessage = 'Okie Dokie';
-        res.write('<h1>very simple http server</h1>');
-        let response = '';
-
-        if (req.method === 'GET') {
-            response += `you wanted to get ${req.url}`;
-        }
-        else {            
-            res.write('hello world');
-        }
-        response += 'have a nice day!';
-
-        res.end(response);
-    }
-});
+const server = http.createServer(app);
 
 const port = 3000;
 server.listen(port, () => {
--- tests/app.test.js
+++ tests/app.test.js
@@ -8,13 +8,36 @@ const app = require('../lib/app');
 describe('server', () => {
     const request = chai.request(app);
 
-    it('says hello world', done => {
-        request.get('/')
+    it('says hello stranger when no name specified', done => {
+        request.get('/greeting')
             .end((err, res) => {
                 if (err) return done(err);
-                console.log('response: ', res.text);
-                assert.equal(res.text, 'hello world');
+                assert.equal(res.text, 'hello stranger');
                 done();
             });
     });
+
+    it('says hello monica when name monica is specified', done => {
+
+        request.get('/greeting/monica')
+            // request.get('/greeting/jane?salutation=yo')
+            .end((err, res) => {
+                if (err) return done(err);
+                assert.equal(res.text, 'hello monica');
+                done();
+            });
+    });
+
+    it('says yo monica when name monica is specified with yo salutation', done => {
+
+        request.get('/greeting/monica?salutation=yo')
+
+            .end((err, res) => {
+                if (err) return done(err);
+                assert.equal(res.text, 'yo monica');
+                done();
+
+            });
+    });
+
 });
