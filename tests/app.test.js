91a63ba7c8a7e34b3ea5035b297d8cc7279cb138
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-18 15:44:16 -0700

    failed tests, lots of console logs

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
 const chai = require('chai');
+const assert = chai.assert;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const app = require('../lib/app');
@@ -7,39 +7,46 @@ const cowsay = require('cowsay');
 
 describe('app', () => {
     const request = chai.request(app);
-    
+
     it('greets a stranger', done => {
         request.get('/greeting')
             .end((err, res) => {
-                assert.equal(res.text, cowsay.say({text: 'Hello stranger'}));
+                assert.equal(res.text, cowsay.say({ text: 'Hello stranger' }));
                 done();
             });
     });
     it('greets by name', done => {
         request.get('/greeting/Joe')
             .end((err, res) => {
-                assert.equal(res.text, cowsay.say({text: 'Hello Joe'}));
+                assert.equal(res.text, cowsay.say({ text: 'Hello Joe' }));
                 done();
             });
     });
-    it('greets a stranger', done => {
+    it('custom greets a stranger', done => {
         request.get('/greeting?salutation=Hola')
             .end((err, res) => {
-                assert.equal(res.text, cowsay.say({text: 'Hola stranger'}));
+                assert.equal(res.text, cowsay.say({ text: 'Hola stranger' }));
                 done();
             });
     });
     it('custom greets by name', done => {
         request.get('/greeting/Joe?salutation=Yo')
             .end((err, res) => {
-                assert.equal(res.text, cowsay.say({text: 'Yo Joe'}));
+                assert.equal(res.text, cowsay.say({ text: 'Yo Joe' }));
                 done();
             });
     });
     it('asks for facts', done => {
         request.get('/fact')
             .end((err, res) => {
-                assert.ok(res.text);
+                assert.include(res.text, 'FACT:');
+                done();
+            });
+    });
+    it('posts to logs', done => {
+        request.post('/logs')
+            .end((err, res) => {
+                assert.equal(res.text, 'bar');
                 done();
             });
     });
