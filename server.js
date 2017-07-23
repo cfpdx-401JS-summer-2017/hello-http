6c6ebe23afd25bcc2054585ff5bcc70c9aa8287f
Wendy Beck <webeck@gmail.com>
2017-07-22 22:21:07 -0700

    playing around with eslint and starting to promisify

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const http = require('http');
+const promisify = require('promisify').util();
+const http = promisify(require('http'));
 const app = require('./lib/app.js');
 
 const server = http.createServer(app);
 const port = 2000;
 
 server.listen(port, err => {
-	console.log('server listening on port: ', server.address());
-	if (err) {
-		return console.log('there was an error', err);
-	}
+  console.log('server listening on port: ', server.address());
+  if (err) {
+    return console.log('there was an error', err);
+  }
 });
