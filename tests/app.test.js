3c27984f129f0d0932a6cd6381201f4098957ff7
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-08-03 18:24:24 -0700

    first(?) commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,19 +1,36 @@
 const chai = require('chai');
-const assert = chai.assert;
 const http = require('http');
 const chaiHttp = require('chai-http');
+const assert = chai.assert;
 chai.use(chaiHttp);
-const app = require('./lib/app');
+const app = require('../lib/app');
 
 describe('server requests', () => {
     const request = chai.request(app);
 
-    it('works', done => {
+    it('works without specification', done => {
+        request.get('/greeting')
+            .end((err, res) => {
+                if(err) done(err);
+                assert.equal(res.text, 'hello stranger');
+                done();
+            });
+    });
+
+    it('returns greeting with name', done => {
         request.get('/greeting/joe')
             .end((err, res) => {
-                if (err) done(err);
+                if(err) done(err);
                 assert.equal(res.text, 'hello joe');
                 done();
+            });
+    });
+
+    it('returns greeting and salutation', done => {
+        request.get('/greeting/joe?salutation=sup')
+            .end((err, res) => {
+                if(err) done(err);
+                assert.equal(res.text, 'sup joe');
             });
     });
 });
